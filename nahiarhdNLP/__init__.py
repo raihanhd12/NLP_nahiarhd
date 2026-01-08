b10f9585a5311e9ee17f19a1f0fe51b8fab8de54
Raihan Hidayatullah Djunaedi <fashfour487@gmail.com>
2026-01-08 13:28:46 +0700

    fix: update version to 1.5.3 in __init__.py and pyproject.toml; remove obsolete test files

--- nahiarhdNLP/__init__.py
+++ nahiarhdNLP/__init__.py
@@ -6,7 +6,7 @@ nahiarhdNLP - Advanced Indonesian Natural Language Processing Library
 from . import datasets, preprocessing
 
 # Version info
-__version__ = "1.5.2"
+__version__ = "1.5.3"
 __author__ = "Raihan Hidayatullah Djunaedi"
 __email__ = "raihanhd.dev@gmail.com"
 
